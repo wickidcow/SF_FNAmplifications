d8b411e053f6a1e621f0db08b1cc1520affff4dd
wickidcow <jaywago@gmail.com>
2026-09-24 18:30:25 -0400

    Use cross-version smoke effect

--- src/main/java/ne/fnfal113/fnamplifications/multiblocks/FnGemUpgrader.java
+++ src/main/java/ne/fnfal113/fnamplifications/multiblocks/FnGemUpgrader.java
@@ -153,7 +153,7 @@ public class FnGemUpgrader extends MultiBlockMachine {
         }
 
         Bukkit.getScheduler().runTaskLater(FNAmplifications.getInstance(), () -> {
-            b.getWorld().playEffect(b.getLocation().add(0.5, 0.7, 0.5), Effect.SMOKE, 1);
+            b.getWorld().playEffect(b.getLocation().add(0.5, 0.7, 0.5), ne.fnfal113.fnamplifications.utils.Utils.SMOKE_EFFECT, 1);
             b.getWorld().spawnParticle(VersionedParticle.SMOKE, b.getLocation().add(0.3, 1.7, 0.45), 2, 0.1, 0.1, 0.1, 0.1);
             b.getWorld().playSound(b.getLocation(), Sound.BLOCK_ANVIL_PLACE, 1, 1);
 
@@ -168,7 +168,7 @@ public class FnGemUpgrader extends MultiBlockMachine {
                     b.getWorld().playSound(b.getLocation(), Sound.BLOCK_ANVIL_USE, 1, 1);
 
                     Bukkit.getScheduler().runTaskLater(FNAmplifications.getInstance(), () -> {
-                        b.getWorld().playEffect(b.getLocation().add(0.5, 0.7, 0.5), Effect.SMOKE, 1);
+                        b.getWorld().playEffect(b.getLocation().add(0.5, 0.7, 0.5), ne.fnfal113.fnamplifications.utils.Utils.SMOKE_EFFECT, 1);
                         b.getWorld().spawnParticle(Particle.FLASH, b.getLocation().add(0.35, 0.4, 0.4), 2, 0.1, 0.1, 0.1, 0.1);
                         b.getWorld().spawnParticle(Particle.CLOUD, b.getLocation().add(0.35, 0.5, 0.4), 2, 0.1, 0.1, 0.1, 0.1);
                         b.getWorld().playSound(b.getLocation(), Sound.BLOCK_ANVIL_USE, 1, 1);
